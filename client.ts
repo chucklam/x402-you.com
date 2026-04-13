5a0f66b90b4da8c9e5f95009c812fefd2f7f7796
Chuck Lam <939400+chucklam@users.noreply.github.com>
2026-04-13 16:24:28 +0700

    Accept query string from command line argument in client.ts

--- client.ts
+++ client.ts
@@ -1,7 +1,8 @@
 import 'dotenv/config'
 
 const baseUrl = process.env.URL
-const query = encodeURIComponent('What are the latest news in quantum computing')
+const queryString = process.argv[2] || 'What are the latest news in quantum computing'
+const query = encodeURIComponent(queryString)
 const response = await fetch(`${baseUrl}/research?q=${query}`)
 const data = await response.json()
 console.log(data)
