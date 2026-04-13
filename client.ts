a4c15a22a75f6623fab99bc8001ce5595da9ea43
Chuck Lam <939400+chucklam@users.noreply.github.com>
2026-04-13 16:13:28 +0700

    Add route that takes query parameter `q`

--- client.ts
+++ client.ts
@@ -1,1 +1,7 @@
-console.log('Hello World!')
+import 'dotenv/config'
+
+const baseUrl = process.env.URL
+const query = encodeURIComponent('What are the latest news in quantum computing')
+const response = await fetch(`${baseUrl}/research?q=${query}`)
+const data = await response.json()
+console.log(data)
--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,9 @@ app.get('/', (_req, res) => {
   res.send(`Payment will be on ${process.env.NETWORK}`)
 })
 
-app.get('/api/users/:id', (_req, res) => {
-  res.json({ id: _req.params.id })
-})
-
-app.get('/api/posts/:postId/comments/:commentId', (_req, res) => {
-  res.json({ postId: _req.params.postId, commentId: _req.params.commentId })
+app.get('/research', (req, res) => {
+  const { q } = req.query
+  res.json({ query: q })
 })
 
 export default app
